cb1ae7afbe9a465e109c5418778f7f92e503f13f
User <guglmoj67@gmail.com>
2022-09-17 15:45:29 +0300

    First commit

---
+++ src/DOCHandler.java
@@ -0,0 +1,21 @@
+public class DOCHandler extends  AbstractHandler{
+    @Override
+    public void open() {
+
+    }
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void change() {
+
+    }
+
+    @Override
+    public void save() {
+
+    }
+}
---
+++ src/XMLHandler.java
@@ -0,0 +1,21 @@
+public class XMLHandler extends AbstractHandler{
+    @Override
+    public void open() {
+
+    }
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void change() {
+
+    }
+
+    @Override
+    public void save() {
+
+    }
+}
